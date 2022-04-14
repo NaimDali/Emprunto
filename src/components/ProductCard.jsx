6bb3427cbcebe68b2c1ec9570ebad7c2994e33bf
HediKhemiri3001 <khemirimedhedi@gmail.com>
2022-04-14 16:47:23 +0100

    Initial search component commit, needs styling and testing.

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,24 @@
+import styles from "..styles/ProductCard.module.css";
+
+export default function ProductCard({ product }) {
+  return (
+    <>
+      <div className={styles["card"]} onClick="">
+        <img
+          src={product.thumbnail}
+          className={styles["card_thumbnail"]}
+          alt={`${product.name}'s thumbnail.`}
+        ></img>
+        <h1 className={styles["card_header"]}>{product.name}</h1>
+        <sub className={styles["card_tags"]}>
+          {product.categories.map((category) => category.name + " ")}
+        </sub>
+        {product.availability ? (
+          <h4 style={"color: green"}>Available</h4>
+        ) : (
+          <h4 style={"color :red"}>Not Available</h4>
+        )}
+      </div>
+    </>
+  );
+}
