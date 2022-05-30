6212effd333880c8f2d0af2ab3e86ea23897d1cc
HediKhemiri3001 <khemirimedhedi@gmail.com>
2022-05-30 08:27:02 +0100

    Fixing axios

--- src/pages/Laptops.jsx
+++ src/pages/Laptops.jsx
@@ -1,13 +1,13 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Article from "../components/Article";
 
 export default function Laptops() {
   const [Product, setProduct] = useState({});
 
-  const getProduct = () => {
+  useEffect(() => {
     axios
-      .get("https://localhost:3000/products/20")
+      .get("http://localhost:4000/products/20")
       .then((res) => {
         console.log("******************************");
         console.log(res.data);
@@ -16,12 +16,11 @@ export default function Laptops() {
       .catch((err) => {
         console.log(err);
       });
-  };
+  });
 
   return (
     <div class="container">
       <h2> product details: </h2>
-      {getProduct()}
       <ul>
         <li> id= {Product.id}</li>
         <li> name= {Product.name}</li>
